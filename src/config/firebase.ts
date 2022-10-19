949e2f5e2d4c5a37d5074715c6e3c3c1ca7dad96
DIMMOY <dimmy6789@gmail.com>
2022-10-20 04:08:52 +0700

    set up firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,22 @@
+
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+
+  
+export const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FB_APIKEY,
+    authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FB_PROJECTID,
+    storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FB_APPID,
+    measurementId: process.env.REACT_APP_FB_MEASUREMENTID,
+    };
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+// export const firebaseAnalytics = await getAnalytics(firebaseApp);
+
+// Firebase auth
+export const firebaseAuth = getAuth(firebaseApp);
