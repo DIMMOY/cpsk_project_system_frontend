15339702bb090ea28b008f37f79dcc39627744af
DIMMOY <dimmy6789@gmail.com>
2023-01-02 14:43:43 +0700

    edit style

--- src/styles/layout/_preview/_previewCommon.ts
+++ src/styles/layout/_preview/_previewCommon.ts
@@ -2,7 +2,6 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box"
 
 export const CommonPreviewContainer = styled(Box)({
-    width: 'calc(100% - 8vw)',
     margin: '0 auto',
     padding: '1.5rem 4vw 1.5rem 4vw',
     position: 'static',
